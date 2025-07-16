4d764756bb96d2ef9ce99dc8aa9156e59e6cc515
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-16 15:47:46 -0300

    feat: integers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-const TOUCHDOWN_POINTS: i32 = 6;
 #[allow(unused_variables)]
 fn main() {
-    let season: &str = "Verano";
-    let event_time: &str = "06:00";
-    let event_time: i32 = 6;
-    let mut points_scored: i32 = 28;
-    let _favorite_beverage: &str = "Guarana";
+    let eight_bit:i8 = -128;
+    let eight_bit:u8 = 255;
+    let sixteen_bit_signed: i16 = 32500;
+    let thirty_two_bit_signed: i32 = -2147483648;
+    let thirty_two_bit_unsigned: u32 = 4294967295;
 
-    println!("Values: {season} {event_time} {points_scored}");
-
-    points_scored = 35;
-
-    println!("Values: {season} {event_time} {points_scored} {TOUCHDOWN_POINTS}");
+    let some_value  = 20u8;
+    
+    println!("Some value: {some_value}")
 }
