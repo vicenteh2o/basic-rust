0ff9ccea4e6b1c07b65d30e56337d402f4786e6e
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-16 11:19:44 -0300

    feat: dinamic strings

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     let apples = 50;
     let oranges = 14 + 6;
     let fruits = apples + oranges;
+
+    println!("This year, my garden has {fruits} fruits.");
+    println!("Fruit 1 has {1} fruit 2 has {0}. Total: {0} + {1}", apples, oranges);
 }
