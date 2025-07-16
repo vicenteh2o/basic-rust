4cbce63e6277f3b68b32de7edb9a55ccdf82d04c
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-16 13:22:11 -0300

    feat: compiler directives

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 type Meters = i32;
 
+// compile directive
+#[allow(unused_variables)]
 fn main() {
     let mile_race_length: Meters = 1600;
     let miles_race_length: Meters = 1000;
-    println!("All data: {mile_race_length} and {miles_race_length}")
 }
