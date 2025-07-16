f56589df2f74d9c9cc20e2a4d3eb6f93535531aa
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-16 13:48:33 -0300

    feat: const, mut, directives etc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-type Meters = i32;
-
-// compile directive
+const TOUCHDOWN_POINTS: i32 = 6;
 #[allow(unused_variables)]
 fn main() {
-    let mile_race_length: Meters = 1600;
-    let miles_race_length: Meters = 1000;
+    let season: &str = "Verano";
+    let event_time: &str = "06:00";
+    let event_time: i32 = 6;
+    let mut points_scored: i32 = 28;
+    let _favorite_beverage: &str = "Guarana";
+
+    println!("Values: {season} {event_time} {points_scored}");
+
+    points_scored = 35;
+
+    println!("Values: {season} {event_time} {points_scored} {TOUCHDOWN_POINTS}");
 }
