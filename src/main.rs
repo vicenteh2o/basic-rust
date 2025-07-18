a42d183649e9c9a4efa39d52acfef8755b822ed4
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-07-18 15:34:22 -0300

    feat: ownership, reference and deref

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,31 @@ fn main() {
     println!("Candy: {candy}");
     candy.push_str(" forever");
     println!("Candy: {candy}");
+
+    // Moves and ownership
+    let person = String::from("Vicen");
+    // genius new ownership of the heap allocation
+    // person become out of scope
+    let genius = person;
+    // free memory allocation from heap, not stack
+    drop(genius);
+
+    // clone: creates 2 copies on heap
+    let name_id = String::from("id001");
+    let copy_name = name_id.clone();
+
+    println!("Name ID: {name_id}");
+
+    // references and borrowing &
+    let my_stack_value = 2;
+    let my_int_ref = &my_stack_value;
+    println!("Reference value: {my_int_ref}");
+
+    let my_heap_value = String::from("Toyota");
+    let my_heap_reference = &my_heap_value;
+
+    // dereference operator: follows an address
+    println!("De-Reference value: {}", *my_int_ref);
+
+    // String, str and &String, &str
 }
